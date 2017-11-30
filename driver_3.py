0ce67ed9f88eede0a14a1cebfb51df8801f6db2e
Mikhail <mikhail.berezovskiy@gmail.com>
2017-11-30 13:10:11 -0500

    50%?

--- driver_3.py
+++ driver_3.py
@@ -1,15 +1,14 @@
 import sys
 import csv
 import math
-from copy import deepcopy
 
-
-class init:
+class sudoku:
     def __init__(self):
         self.testing_sequences = self.load_file()
         self.testing_board_str = self.testing_sequences[1]
-        self.testing_board_di = self.convert_str_to_dict(self.testing_board_str)
 
+        for each in self.testing_sequences:
+            print (self.solve(each))
 
     def load_file(self):
         # load examples from start csv
@@ -24,7 +23,7 @@ class init:
     def convert_str_to_dict(self, string_board):
         # convert string board to dict
         # format: square 9x9 cells with "A-I" for x and "1-9" for Y
-        print (string_board)
+        # print (string_board)
         di = {}
         for i in range(len(string_board)):
             # x_val = chr(65 + math.trunc(i/9))
@@ -33,7 +32,7 @@ class init:
             sq_val = int(i/27)*3 + int(i%9/3) 
             di_prop = map(str,[x_val,y_val,sq_val])
             di["".join(di_prop)] = int(string_board[i])
-        print (di)
+        # print (di)
         return di
 
     def print_board(sefl, string_board):
@@ -47,44 +46,79 @@ class init:
         # Try AC-3 first, if pass return result
         # Else try bts
         # convert str board to dict before solving
-        X = self.board_str_to_dict(string_board)
-        D = list(range(1,10))
+        X = self.convert_str_to_dict(string_board)
         # try AC-3
-        test_ac3 = self.ac3(X, D)
+        test_ac3 = self.ac3(X)
+        # print (test_ac3)
         if test_ac3["Pass"]:
-            return (test_ac3["Board"], "AC3")
-        # try bts
+            return (string_board, "AC3")
         else:
-            test_bts = self.bts(X)
-            return (test_bts["board"], "BTS")
+            return (string_board, "Not Pass")
+        # try bts
+        # else:
+        #     test_bts = self.bts(X)
+        #     return (test_bts["board"], "BTS")
 
 
-
-    def get_actions(self, X):
+    def unary_constraints(self, X):
         # input all cells
         # output dict with avail cell actions
+        self.q = []
         D = {}
         for cell in X:
             avail_nums = list(range(1,10))
-            D[cell] = []
-            for i in X:
-                if cell[0] == i[0] or cell[1] == i[1] or cell[2] == i[2]:
-                    try:
-                        avail_nums.remove(X[i])
-                    except:
-                        pass        
-            print (cell, X[cell], avail_nums)
-            D[cell] = avail_nums
-        print (D)
+            if X[cell] != 0:
+                D[cell] = [X[cell]]
+            else:
+                D[cell] = []
+                for i in X:
+                    if cell[0] == i[0] or cell[1] == i[1] or cell[2] == i[2]:
+                        try:
+                            self.q.append([cell, i])
+                            avail_nums.remove(X[i])
+                        except:
+                            pass        
+                D[cell] = avail_nums
+        return D 
 
-    def test_constraints(self, X, cell):
-        for i in X:
-            if cell[0] == i[0] or cell[1] == i[1] or cell[2] == i[2]:
-                    
+    def arc_reduce(self, x, y, Dx, Dy):
+        # input x, y and their available domains
+        # output filtered out domain X
+        # c1: AllDiff
+        # Choose vx that way that vx and vy are consistant with c1 
+        # Iterate until len Dx == 1, remove 
+        change = False
 
-    def test_ac3(self, X, D, R1, R2):
-        return True
+
+        return change
+
+    def ac3(self, X):
+        # return unary constraints
+        self.D = self.unary_constraints(X)
+        q = self.q
+
+        while len(q) != 0:
+            # print (len(q))
+            arc = q[0]
+            x,y = arc
+            q.remove([x,y])
+            if X[x] != 0:
+                next
+            if self.arc_reduce(x,y,self.D[x],self.D[y]):
+                if len(self.D[x]) == 0:
+                    return {"Pass": False, "Board": self.D} 
+                # else:
+                #     for i in range(9):
+                #         b = str(i)
+                #         q.append([b+x[1:3], x])
+                #         q.append([x[0] + b + x[2],x])
+                #         q.append([x[0:2] + b, x])
+
+        return {"Pass": True, "Board": self.D}
+    
+    
     # init ac-3
+
 
     #  Input:
     #    A set of variables X
@@ -123,7 +157,6 @@ class init:
     #      return change
 
 
-init = init()
-st = init.testing_sequences[1]
-init.print_board(st)
-D = init.get_actions(init.testing_board_di)
+sudoku = sudoku()
+# string_board = sudoku.testing_board_str
+# sudoku.solve(string_board)
